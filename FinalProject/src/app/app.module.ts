f4205e7a0dae3e52fa82c4c8173ed73cf8f1b55a
kdemello1980 <k6demello28@fastmail.fm>
2020-07-13 16:39:31 -0400

    scaffolded select-task and update-task components

--- FinalProject/src/app/app.module.ts
+++ FinalProject/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UpdateTaskComponent } from './update-task/update-task.component';
+import { SelectTaskComponent } from './select-task/select-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpdateTaskComponent,
+    SelectTaskComponent
   ],
   imports: [
     BrowserModule,
