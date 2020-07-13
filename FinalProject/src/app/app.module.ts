120ec42a42c932d98b82125f8804d66b8ad52eb5
Siegfried <65792191+csieg150@users.noreply.github.com>
2020-07-13 16:04:33 -0500

    added todo service, added dependencies to module

--- FinalProject/src/app/app.module.ts
+++ FinalProject/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { TodosService } from './services/todos.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [TodosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
