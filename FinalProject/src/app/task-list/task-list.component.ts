e98ffe5f4e40903d94da8d9e2a20d7870b74d4fe
nKlapatch <52015405+nKlapatch@users.noreply.github.com>
2020-07-14 13:14:58 -0500

    Add the task list

--- FinalProject/src/app/task-list/task-list.component.ts
+++ FinalProject/src/app/task-list/task-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {TodosService} from '../services/todos.service';
+import {ITask} from './task';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-task-list',
@@ -9,16 +11,29 @@ import {TodosService} from '../services/todos.service';
 export class TaskListComponent implements OnInit {
 
   pageTitle = 'Task List';
-  constructor(private todos: TodosService) { }
+  allTodos: ITask[];
+  filteredTodos: ITask[];
+  
+  constructor(private todos: TodosService) {
+    this.getAllTodos();
+  }
 
   ngOnInit(): void {
   }
 
-  getAllTodos() {
+  getAllTodos(): ITask[] {
+    this.allTodos = [];
     this.todos.getTodos().subscribe(
       response => {
-        console.log(response);
+        for(let todo of response) {
+          let todoString = JSON.stringify(todo);
+          this.allTodos.push(JSON.parse(todoString));
+        }
+        console.log(this.allTodos);
+        
       }
     );
+    this.filteredTodos = this.allTodos;
+    return this.allTodos;
   }
 }
