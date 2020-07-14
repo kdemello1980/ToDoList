8e038ceba697f9e8d0bd563c99211ff57a127e98
kdemello1980 <k6demello28@fastmail.fm>
2020-07-14 12:13:42 -0400

    separate imports into modules, components, and services.  Added UpdateTaskService & SelectTaskService to app.module.ts

--- FinalProject/src/app/app.module.ts
+++ FinalProject/src/app/app.module.ts
@@ -1,12 +1,25 @@
+/**
+ * Modules
+ */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
-import { TodosService } from './services/todos.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/**
+ * Components
+ */
+import { AppComponent } from './app.component';
 import { UpdateTaskComponent } from './update-task/update-task.component';
 import { SelectTaskComponent } from './select-task/select-task.component';
+
+/**
+ * Services
+ */
+import { TodosService } from './services/todos.service';
+import { UpdateTaskService } from './services/update-task.service';
+import { SelectTaskService } from './services/select-task.service';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +32,7 @@ import { SelectTaskComponent } from './select-task/select-task.component';
     HttpClientModule,
     BrowserAnimationsModule
   ],
-  providers: [TodosService],
+  providers: [TodosService, UpdateTaskService, SelectTaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
