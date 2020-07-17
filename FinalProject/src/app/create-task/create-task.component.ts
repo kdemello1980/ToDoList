ba3aaa9294a16830382acb890659c6f21465549a
Siegfried <65792191+csieg150@users.noreply.github.com>
2020-07-17 12:30:01 -0500

    removed dialog component, create task works

--- FinalProject/src/app/create-task/create-task.component.ts
+++ FinalProject/src/app/create-task/create-task.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import { TodosService } from '../services/todos.service';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-create-task',
@@ -8,27 +9,39 @@ import { TodosService } from '../services/todos.service';
   styleUrls: ['./create-task.component.css']
 })
 export class CreateTaskComponent implements OnInit {
+  inputTitle = new FormControl('', [Validators.required]);
+  inputComplete = new FormControl('', [Validators.required]);
+  inputDescription = new FormControl('', [Validators.required]);
+  defaultValue = 'No';
 
   constructor(public dialog: MatDialog, private todoServ: TodosService) { }
 
-  openDialog(): void {
-    const dialogRef = this.dialog.open(CreateDialogComponent);
+  // openDialog(): void { // Here in case we decided to swap to dialog
+  //   const dialogRef = this.dialog.open(CreateDialogComponent);
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
+  //   dialogRef.afterClosed().subscribe(result => {
+  //     console.log(`Dialog result: ${result}`);
+  //   });
+  // }
 
   createTask(): void{
-    const taskComplete = (document.getElementById('taskComplete').innerText === 'completed') ?
+    const taskTitle = (document.getElementById('taskTitle') as HTMLInputElement).value;
+    const taskComplete = ((document.getElementById('taskComplete') as HTMLInputElement).value === 'Yes') ?
       true : false;
+    const taskDescription = (document.getElementById('taskDescription') as HTMLInputElement).value;
 
+    if (!taskTitle || !taskDescription){
+      console.log(`${taskTitle} ${taskComplete} ${taskDescription}`);
+      this.getErrorMessage(this.inputTitle);
+      return;
+    }
+    // format used to 'post' to the api
     const taskForm = {
       completed: taskComplete,
-      title: document.getElementById('taskTitle').innerText,
-      description: document.getElementById('taskDescription').innerText
+      title: taskTitle,
+      description: taskDescription
     };
-
+    console.log(taskForm);
     this.todoServ.postTodo(taskForm).subscribe(
       response => {
         console.log(response);
@@ -37,13 +50,14 @@ export class CreateTaskComponent implements OnInit {
 
   }
 
+  getErrorMessage(input: FormControl): string{
+    if (input.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return;
+  }
+
   ngOnInit(): void {
   }
 
 }
-
-@Component({
-  selector: 'app-create-task-dialog',
-  templateUrl: 'create-task-dialog.html',
-})
-export class CreateDialogComponent {}
