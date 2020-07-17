34640672996a3a1e587c2be383f733283bdc98d8
Siegfried <65792191+csieg150@users.noreply.github.com>
2020-07-17 10:18:44 -0500

    fixed create task component

--- FinalProject/src/app/create-task/create-task.component.ts
+++ FinalProject/src/app/create-task/create-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import { TodosService } from '../services/todos.service';
 
 @Component({
   selector: 'app-create-task',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTaskComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog, private todoServ: TodosService) { }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(CreateDialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  createTask(): void{
+    const taskComplete = (document.getElementById('taskComplete').innerText === 'completed') ?
+      true : false;
+
+    const taskForm = {
+      completed: taskComplete,
+      title: document.getElementById('taskTitle').innerText,
+      description: document.getElementById('taskDescription').innerText
+    };
+
+    this.todoServ.postTodo(taskForm).subscribe(
+      response => {
+        console.log(response);
+      }
+    );
+
+  }
 
   ngOnInit(): void {
   }
 
 }
+
+@Component({
+  selector: 'app-create-task-dialog',
+  templateUrl: 'create-task-dialog.html',
+})
+export class CreateDialogComponent {}
